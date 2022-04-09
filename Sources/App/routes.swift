8a8258dd2a2e5c6d7e8d1db2431eee98af04c734
waldrumpus <waldrumpus@gmail.com>
2022-04-09 15:24:58 +0200

    add cors

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,7 +2,7 @@ import Vapor
 
 struct WhaRequest: Codable, Content {
     let guess: String
-    let state: State
+    let state: State?
 }
 
 struct WhaResponse: Codable, Content {
@@ -18,15 +18,13 @@ var words: [String] = []
 
 func routes(_ app: Application) throws {
     
+    //TODO: put this in a better place
     let path = URL(fileURLWithPath: app.directory.resourcesDirectory).appendingPathComponent("words.txt").path
-    
     words = try! String(contentsOfFile: path).split(separator: "\n").map {String($0)}
-    
-    print(app.directory.resourcesDirectory)
-    
+        
     app.post { req -> WhaResponse in
         let request = try req.content.decode(WhaRequest.self)
-        var state = request.state
+        var state = request.state ?? State()
         guard let result = state.chooseWord(matching: request.guess, from: words) else {
             throw WhaError(message: "could not find a word")
         }
